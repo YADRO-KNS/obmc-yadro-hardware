b08a0712e7e2350d9bcb1f980e2325f2e43dbfaa
Igor Kononenko <i.kononenko@yadro.com>
2021-12-03 00:19:07 +0300

    Add network-adapters inventory manager.
    The appropriate service provides objects with the
    com.Yadro.NetworkAdapter interface.
    
     Example:
    ```
    busctl introspect com.yadro.NetworkAdapter
           /xyz/openbmc_project/inventory/system/network/adapter/eth0
    
    NAME                                TYPE      SIGNATURE RESULT/VALUE
    org.freedesktop.DBus.Introspectable interface -         -
    .Introspect                         method    -         s
    org.freedesktop.DBus.Peer           interface -         -
    .GetMachineId                       method    -         s
    .Ping                               method    -         -
    org.freedesktop.DBus.Properties     interface -         -
    .Get                                method    ss        v
    .GetAll                             method    s         a{sv}
    .Set                                method    ssv       -
    .PropertiesChanged                  signal    sa{sv}as  -
    xyz...Decorator.Asset               interface -         -
    .BuildDate                          property  s         ""
    .Manufacturer                       property  s         "Intel Corporation"
    .Model                              property  s         "I210 Gigabit Network Connection"
    .PartNumber                         property  s         ""
    .SerialNumber                       property  s         ""
    xyz.openbmc_project.Inventory.Item  interface -         -
    .Present                            property  b         true
    .PrettyName                         property  s         "eth0"
    xyz...NetworkInterface              interface -         -
    .MACAddress                         property  s         "a0:c5:f2:10:28:3c"
    xyz...OperationalStatus             interface -         -
    .Functional                         property  b         true
    
    ```
    
    End-user-impact: None
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

---
+++ src/network/adapter.cpp
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2021 YADRO.
+ */
+
+#include "adapter.hpp"
+
+#include "dbus.hpp"
+#include "pcidb.hpp"
+
+#include <phosphor-logging/log.hpp>
+
+#include <charconv>
+#include <fstream>
+
+using namespace phosphor::logging;
+
+const std::string NetworkAdapter::inventoryPath =
+    dbus::inventory::pathBase + std::string("/system/network/adapter/");
+
+NetworkAdapter::NetworkAdapter(sdbusplus::bus::bus& bus,
+                               const std::string& name,
+                               const std::string& vendor,
+                               const std::string& device,
+                               const std::string& macAddress) :
+    InventoryItemServer(bus, dbusEscape(inventoryPath + name).c_str()),
+    NetworkInterfaceServer(bus, dbusEscape(inventoryPath + name).c_str()),
+    DecoratorAssetServer(bus, dbusEscape(inventoryPath + name).c_str()),
+    OperationalStatusServer(bus, dbusEscape(inventoryPath + name).c_str())
+{
+    // try to render adapter manufacturer/model (use pci.ids database)
+    auto [vendorName, modelName] = pciLookup(vendor, device);
+    // xyz.openbmc_project.Inventory.Item
+    prettyName(name);
+    present(true);
+    // xyz.openbmc_project.Inventory.Item.NetworkInterface
+    mACAddress(macAddress);
+    // xyz.openbmc_project.Inventory.Decorator.Asset
+    manufacturer(vendorName);
+    model(modelName);
+    // xyz.openbmc_project.State.Decorator.OperationalStatus
+    functional(true);
+}
